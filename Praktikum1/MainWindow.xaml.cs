73201469d7cb80c41ae422ec9b57298ce6f0794d
TheAlexGo <alex280702@mail.ru>
2021-09-25 13:45:11 +0300

    Выполнение первого задания

--- Praktikum1/MainWindow.xaml.cs
+++ Praktikum1/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Praktikum1
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
         {
